cc1d669bcc01f91d599250f19830cb5d001d56a9
MohammadJavadJourney <md.gholinejad@gmail.com>
2024-11-24 01:02:32 +0330

    creating submission

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,35 +1,41 @@
-'use client';
+"use client";
 
-import Link from 'next/link'
-import { usePathname } from 'next/navigation';
-import React from 'react'
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 import { BsFillBugFill } from "react-icons/bs";
-import classNames from 'classnames';
+import classNames from "classnames";
 
 const NavBar = () => {
-
   const currentPath = usePathname();
-  console.log(currentPath)
 
   const links = [
-    { label: 'Dashboard', href: '/' },
-    { label: 'Issues', href: '/issues' }
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
   ];
 
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href='/'><BsFillBugFill /></Link>
-        <ul className='flex space-x-6'>
-            { links.map(link => 
-            <Link key={link.href} href={link.href} className={classNames({
-              'text-zinc-900': link.href === currentPath,
-              'text-zinc-400': link.href !== currentPath,
-              'hover: text-zinc-500 transition-colors': true
-            })}>
-              {link.label}</Link>) }
-        </ul>
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <BsFillBugFill />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className={classNames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-400": link.href !== currentPath,
+              "hover:text-zinc-600 transition-colors": true,
+            })}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
