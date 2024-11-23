59031e2145cbcb3c99aa457c335b020ec0fdfc3a
MohammadJavadJourney <md.gholinejad@gmail.com>
2024-11-23 20:41:09 +0330

    added active styles to navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,15 @@
+'use client';
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { BsFillBugFill } from "react-icons/bs";
+import classNames from 'classnames';
 
 const NavBar = () => {
+
+  const currentPath = usePathname();
+  console.log(currentPath)
 
   const links = [
     { label: 'Dashboard', href: '/' },
@@ -14,7 +21,11 @@ const NavBar = () => {
         <Link href='/'><BsFillBugFill /></Link>
         <ul className='flex space-x-6'>
             { links.map(link => 
-            <Link key={link.href} href={link.href} className='text-zinc-500 hover:text-zinc-800 transition-colors'>
+            <Link key={link.href} href={link.href} className={classNames({
+              'text-zinc-900': link.href === currentPath,
+              'text-zinc-400': link.href !== currentPath,
+              'hover: text-zinc-500 transition-colors': true
+            })}>
               {link.label}</Link>) }
         </ul>
     </nav>
