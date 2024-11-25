d1f2d6b3000846ae1e6891720bcc2c9ec8a59b25
MohammadJavadJourney <md.gholinejad@gmail.com>
2024-11-25 20:19:41 +0330

    created initial showing issues page

---
+++ app/components/IssueStatusBadge.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Badge } from "@radix-ui/themes";
+import { Status } from "@prisma/client";
+
+interface Props {
+  status: Status;
+}
+
+const statusMap: Record<
+  Status,
+  { label: String; color: "red" | "green" | "violet" }
+> = {
+  OPEN: { label: "Open", color: "red" },
+  IN_PROGRESS: { label: "In Progress", color: "violet" },
+  CLOSED: { label: "Closed", color: "green" },
+};
+
+const IssueStatusBadge = ({ status }: { status: Status }) => {
+  return (
+    <Badge color={statusMap[status].color}>{statusMap[status].label}</Badge>
+  );
+};
+
+export default IssueStatusBadge;
